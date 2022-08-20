33f5d46f88a64ffcf44702423566fec1e164b5d8
mdrokz <mohammadmunshi@gmail.com>
2022-08-20 16:25:02 +0530

    fix API & write the annotations to labels dir

--- via-node/index.js
+++ via-node/index.js
@@ -1,27 +1,31 @@
-const fastify = require('fastify')({logger: true})
+const fastify = require('fastify')({ logger: true })
+const { writeFileSync } = require('fs');
 const path = require('path')
 
 fastify.register(require('@fastify/static'), {
-  root: path.join(__dirname, 'public'),
-  prefix: '/public/', // optional: default '/'
+    root: path.join(__dirname, 'public'),
 })
 
-fastify.get('/', async (req, reply) => {
-  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+fastify.get('/', function (req, reply) {
+    reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
 })
 
-fastify.post('/api/v2/annotations', async (req, reply) => {
-  reply.send({
-    message: 'Hello World'
-  })
+fastify.post('/api/v2/annotations', function (req, reply) {
+    let body = JSON.parse(req.body);
+
+    console.log(body);
+
+    writeFileSync(`../../labels/${body.fileName}`, body.annotations);
+
+    reply.send("File Saved Successfully");
 })
 
 const start = async () => {
     try {
-      await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 3000 })
     } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
+        fastify.log.error(err)
+        process.exit(1)
     }
-  }
-  start()
+}
+start()
